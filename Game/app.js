65a4b717768e0f43985717e63f76070cfbf46167
Jena Pantano <Jpantano30@gmail.com>
2023-12-20 14:12:03 -0500

    game working, not done...

--- Game/app.js
+++ Game/app.js
@@ -1,90 +1,90 @@
+//constants
 function $(cssSelector) {
     return document.querySelector(cssSelector)
-}
+};
+// 1. global variables 
+const modal = $(`#modal`);
+const closeModal = $(`#playGame`);
+const p1wins = $('#p1wins');
+const ties = $('#ties');
+const p2wins = $('#p2wins');
+const resMsg = $(`#gameovermsg`);
 
-const modal = $(`#modal`)
-const closeModal = $(`#playGame`)
-const wins = $('#wins')
-const ties = $('#ties')
-const losses = $('#losses')
-const game = $(`#Game`)
-const resMsg = $(`#results`)
-const restart = $(`#restart`)
-const gridBoxes = $(`.grid-item`)
-let username = document.getElementById('inputname');
+let players = document.getElementById('inputname');
+const restart = document.getElementById('restart');
+
+const LOOKUP = {
+    '1': 'X', 
+    '-1':'O'
+};
+
+const winningCombos = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+let board;
+let turn;
+let winner;
 
 
 
+//caches dom elements 
+const boardEl = document.getElementById('game-board')
 
 
-
+//event listeners 
 closeModal.addEventListener('click', () => {
-    username.innerText = document.getElementById("insrtname").value;
+    players.innerText = `Player 1: ${document.getElementById("insrtname1").value} vs. Player 2: ${document.getElementById("insrtname2").value}`;
+    players.style.textAlign = 'center'
     modal.style.display = 'none';
-    })
+    });
+boardEl.addEventListener('click', handleClick);
+restart.addEventListener('click', init);
 
+//functions 
+init();
+function init(){
+    board = [null, null, null, null, null, null, null, null, null];
+    turn = 1;
+    winner = null;
+};
 
-closeModal.addEventListener('click', () => {
-modal.style.display = 'none';
-})
-
-const stats = {
-    wins: 0,
-    ties: 0,
-    losses: 0,
-}
-
-
-
-// gridBoxes.addEventListener('click', moveMade)
-
-
-
-
-
-restart.addEventListener('click', () => {
-    resultScreen.style.display = 'none'
-    game.style.display = 'block'
-})
-
-
-// class player {
-//     constructor(name){
-//         this.name = name 
-//     }
-// }
-// const playerX = new player('')
-
-
-
-let movesArr = [null, null, null, null, null, null, null, null, null]
-
-// win if ...
-// winningBoard = [
-//     [0, 1, 2],
-//     [0, 4, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 4, 6],
-//     [2, 5, 6],
-//     [3, 4, 5],
-//     [6, 7, 8]
-// ]
-
-
-function play (){
-
-
-
-
-
-
-
-
-
-    resultScreen.style.display = 'flex'
-
-    wins.children[1].innerText = stats.wins
-    ties.children[1].innerText = stats.ties
-    losses.children[1].innerText = stats.losses
-}
+function handleClick(event){
+    console.log(event.target) 
+    //another player cant make changes to box thats already been clicked 
+    if (board[parseInt(event.target.id)] || winner) return;
+    //next players turn 
+    board[parseInt(event.target.id)] = turn;
+    turn *= -1;
+    console.log(board);
+    winner = checkWinner();
+    render()
+};
+function render() {
+    //if anything in board thats changed, i want to render the box
+    //subfunctions - updatefunction
+    for (let i = 0; i < board.length; i++) {
+        if(board[i]) {
+            const sq = document.getElementById(i);
+            sq.innerHTML = `${LOOKUP[board[i]]}`;
+        }
+    }
+};
+function checkWinner (){
+    for (let i = 0; i < winningCombos.length; i++){
+        sum = Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]);
+        if (sum === 3) {
+            return board[winningCombos[i][0]];
+        }
+    }
+    if (!board.includes(null)) {
+         return 'Tie';
+    }
+    return null;
+};
